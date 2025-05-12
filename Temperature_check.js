86668eb8cb8af199e23ecb1ca8e79f30bff6e70f
chandan-kumar345 <bachandan48@gmail.com>
2025-05-13 01:20:55 +0530

    some changes

--- Temperature_check.js
+++ Temperature_check.js
@@ -1,8 +1,11 @@
-let temprature = 34;
+let temprature = 25;
 
-if( temprature >30)
+if( temprature >30){
     console.log("very hot temperature");
+}    
 else if(  temprature < 30 && temprature > 20)
+{
     console.log("cool & hot temperature");
-else ( temprature <= 10)
+}
+else
     console.log("cool temperature");
