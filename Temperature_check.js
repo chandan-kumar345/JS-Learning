2db605d6962444d3d3202b89679e003d34f26014
chandan-kumar345 <bachandan48@gmail.com>
2025-05-13 01:15:58 +0530

    new file which check temperature

---
+++ Temperature_check.js
@@ -0,0 +1,8 @@
+let temprature = 34;
+
+if( temprature >30)
+    console.log("very hot temperature");
+else if(  temprature < 30 && temprature > 20)
+    console.log("cool & hot temperature");
+else ( temprature <= 10)
+    console.log("cool temperature");
